21dd7bdb3c103ea1eb5b4ee1567b2eb60c87eb4d
benrmatthews <ben@montfort.io>
2014-08-04 19:38:09 +0000

    Ammended community submission form

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,5 +1,6 @@
 class CommunitiesController < ApplicationController
   before_action :set_community, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, :except => [:show, :index]
 
   # GET /communities
   # GET /communities.json
