822b3f50ea0d5dbbfb6fe6b16b1106fbd815fb7c
benrmatthews <ben@montfort.io>
2014-08-05 08:20:47 +0000

    Removed restriction on browse and submit views

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,6 +1,6 @@
 class CommunitiesController < ApplicationController
   before_action :set_community, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, :except => [:show, :index]
+  before_filter :authenticate_user!, :except => [:index, :browse, :new, :create]
 
   # GET /communities
   # GET /communities.json
