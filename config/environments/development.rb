b845a71819e57f9a51c3b59b11f3be68c4b041f5
benrmatthews <ben@montfort.io>
2014-08-06 19:20:57 +0000

    assets compiled for Heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,8 +6,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   config.cache_store = :dalli_store
-
-
+  
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -28,6 +27,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.compile = true
+
 
   config.action_mailer.smtp_settings = {
     address: "smtp.mandrillapp.com",
